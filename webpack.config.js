640737e2cfadcea2e9c8bf6dbba736e9166b9134
Rohan Nobis <nobisro@gmail.com>
2018-11-28 23:17:19 -0500

    added compression plugin to gzip bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 // module.exports = {
 //   entry: `${SRC_DIR}/index.jsx`,
@@ -14,7 +15,7 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 //         test : /\.jsx?/,
 //         include : SRC_DIR,
 //         exclude : 'node_modules',
-//         loader : 'babel-loader',      
+//         loader : 'babel-loader',
 //         query: {
 //           presets: ['react', 'es2015']
 //         }
@@ -24,30 +25,38 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 // };
 
 module.exports = {
-  entry: __dirname + '/client/src/index.js',
+  entry: __dirname + "/client/src/index.js",
   module: {
     rules: [
       {
         test: [/\.js$/, /\.jsx$/],
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+            presets: ["@babel/preset-react", "@babel/preset-env"]
           }
         }
       },
       {
         test: /\.(jpg|png|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[hash].[ext]',
-        },
-    }
+          name: "[path][name].[hash].[ext]"
+        }
+      }
     ]
   },
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/client/dist'
-  }
+    filename: "bundle.js",
+    path: __dirname + "/client/dist"
+  },
+  plugins: [
+    new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 };
